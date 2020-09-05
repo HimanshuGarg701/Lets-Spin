c185f692f26760950562ac8decd3b8bd4ad4b71d
Himanshu Garg <hgarg701@gmail.com>
2020-09-05 00:56:03 -0700

    Creating video footers

--- src/Videos.js
+++ src/Videos.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import "./Video.css";
 
 function Videos() {
+    const videoRef = useRef(null);
+    const [playing, setPlaying] = useState(false);
+
+    const onClickAction = () =>{
+        if(playing){
+            videoRef.current.pause();
+            setPlaying = false;
+        }else{
+            videoRef.current.play();
+            setPlaying = true;
+        }
+    };
+
     return (
         <div className="video">
-            <video src="" loop>
+            <video loop className="video-player" ref={videoRef} onClick={onClickAction()}
+            src="https://www.youtube.com/watch?v=TlpRsSvVCaw">
 
             </video>
         </div>
     )
 }
 
-export default Videos
+export default Videos;
