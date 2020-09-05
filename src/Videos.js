3df2f0f30ec28b2a6d50e36baf068ebd90a5e8fd
Himanshu Garg <hgarg701@gmail.com>
2020-09-04 19:27:40 -0700

    Initialized Lets Spin App

---
+++ src/Videos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Video.css";
+
+function Videos() {
+    return (
+        <div className="video">
+            
+        </div>
+    )
+}
+
+export default Videos
